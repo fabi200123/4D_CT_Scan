c290e8ef3f33848859286044d999ab89c710083a
FabiGamerRO <fabi2001231@yahoo.com>
2023-05-11 00:41:27 +0300

    Scripts for converting Dicom to nrrd
    Added the scripts for converting Dicom to nrrd

---
+++ Scripts_for_dicom_conversion/dicom_to_nrrd.py
@@ -0,0 +1,23 @@
+import os,sys
+sys.path.append("./precision-medicine-toolbox/")
+from pmtool.ToolBox import ToolBox
+import pandas as pd
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sb
+
+parameters = {'data_path': r"C:\\Users\\fabi2\\OneDrive\\Desktop\\Betty's idea of doing shit\\data\\dcms\\", # path to your DICOM data
+              'data_type': 'dcm', # original data format: DICOM
+              'multi_rts_per_pat': False}   # when False, it will look only for 1 rtstruct in the patient folder, 
+                                            # this will speed up the process, 
+                                            # if you have more then 1 rtstruct per patient, set it to True
+
+export_path =r"C:\\Users\\fabi2\\OneDrive\\Desktop\\Betty's idea of doing shit\\data\\" # the function will create 'converted_nrrd' folder in the specified directory
+data_ct = ToolBox(**parameters)
+data_ct.convert_to_nrrd(export_path, 'gtv')
+
+# Generate the 2D PNG files of the nodule
+data_path = "C:\\Users\\fabi2\\OneDrive\\Desktop\\Betty's idea of doing shit\\data\\converted_nrrds\\"
+data_ct_nrrd = ToolBox(data_path, data_type='nrrd')
+data_ct_nrrd.get_jpegs(r"C:\\Users\\fabi2\\OneDrive\\Desktop\\Betty's idea of doing shit\\data\\") # the function will create 'images_quick_check' folder in the specified directory 
