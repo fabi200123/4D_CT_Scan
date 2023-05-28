76c38a469d39dfd3f1d82521c015a55b0a637fe9
FabiGamerRO <fabi2001231@yahoo.com>
2023-05-28 16:28:10 +0300

    Modify the method of calculating FractalDimesion
    Modified the way of calculating the fractal dimension

--- Dash-App/modules/calculate_nodule_dimensions.py
+++ Dash-App/modules/calculate_nodule_dimensions.py
@@ -8,6 +8,9 @@ from PIL import Image
 from radiomics import featureextractor
 import SimpleITK as sitk
 import scipy.spatial.distance as distance
+import numpy as np
+from .FractalDimension import fractal_dimension
+import matplotlib.pyplot as plt
 
 def calculate_nodule_volume(nodule_arr, image):
     non_zero_voxels = np.count_nonzero(nodule_arr)
@@ -16,6 +19,35 @@ def calculate_nodule_volume(nodule_arr, image):
     nodule_volume = non_zero_voxels * voxel_volume
     return nodule_volume
 
+# def calculate_fractal_dimension(nodule_arr):
+#     # Only for 2d image
+#     assert(len(nodule_arr.shape) == 3)
+
+#     # Transform nodule_arr into a binary array
+#     nodule_arr = (nodule_arr > 0)
+
+#     # Minimal and maximal box sizes
+#     sizes = 2**np.arange(3, 10)
+
+#     # Box counting
+#     counts = []
+#     for size in sizes:
+#         count = 0
+#         for x in range(0, nodule_arr.shape[0] - size + 1, size):
+#             for y in range(0, nodule_arr.shape[1] - size + 1, size):
+#                 for z in range(0, nodule_arr.shape[2] - size + 1, size):
+#                     if np.any(nodule_arr[x:x+size, y:y+size, z:z+size]):
+#                         count += 1
+#         counts.append(count)
+
+#     # Add small constant to avoid zero values in log
+#     counts = np.array(counts) + 1e-10
+
+#     # Fit the sizes and counts to a linear equation in log-log scale
+#     coeffs = np.polyfit(np.log(sizes), np.log(counts), 1)
+
+#     return -coeffs[0]
+
 def calculate_fractal_dimension(nodule_arr):
     # Only for 2d image
     assert(len(nodule_arr.shape) == 3)
@@ -23,27 +55,10 @@ def calculate_fractal_dimension(nodule_arr):
     # Transform nodule_arr into a binary array
     nodule_arr = (nodule_arr > 0)
 
-    # Minimal and maximal box sizes
-    sizes = 2**np.arange(3, 10)
+    # Compute the fractal dimension using the module's function
+    fractalDimension = fractal_dimension(nodule_arr)
 
-    # Box counting
-    counts = []
-    for size in sizes:
-        count = 0
-        for x in range(0, nodule_arr.shape[0] - size + 1, size):
-            for y in range(0, nodule_arr.shape[1] - size + 1, size):
-                for z in range(0, nodule_arr.shape[2] - size + 1, size):
-                    if np.any(nodule_arr[x:x+size, y:y+size, z:z+size]):
-                        count += 1
-        counts.append(count)
-
-    # Add small constant to avoid zero values in log
-    counts = np.array(counts) + 1e-10
-
-    # Fit the sizes and counts to a linear equation in log-log scale
-    coeffs = np.polyfit(np.log(sizes), np.log(counts), 1)
-
-    return -coeffs[0]
+    return fractalDimension
 
 def compute_nodule_area(nodule_mask, voxel_spacing):
     area = np.sum(nodule_mask) * voxel_spacing[0] * voxel_spacing[1]
